a86fa69b09f738624d813443ab23cb28ac2a094c
Karan Kumar <118922288+KaranKumar09@users.noreply.github.com>
2026-05-15 10:17:55 +0530

    Add files via upload

---
+++ airthmetic operator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int a;
+		printf("Enter the value of a"); 
+		scanf("%d",&a);
+	int b;
+		printf("Enter the value of b");
+		scanf("%d",&b);
+		printf("%d\n",a+b);
+		printf("%d\n",a-b);
+		printf("%d\n",a*b);
+		float c;
+		c=(float)a/(float)b;
+		printf("%f\n",c); 
+		printf("%d",a%b);
+	
+			
+}
